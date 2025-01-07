586f5e1b62e670fc48bb25384797668cf76783e7
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2025-01-07 15:36:42 +0700

    vitriservice

--- src/services/vitri.service.js
+++ src/services/vitri.service.js
@@ -1,37 +1,10 @@
 import { http } from "./config";
 
-export const vitriService = {
-  vitri: () => {
-    return http.get("/vi-tri");
+export const viTriService = {
+  getViTri: () => {
+    return http.get(`/vi-tri`);
   },
-  themviTri: (values) => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    const token = userInfo?.token;
-    console.log(token);
-    return http.post("/vi-tri", values, {
-      headers: {
-        token: token,
-      },
-    });
-  },
-  xoaviTri: (id) => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    const token = userInfo?.token;
-    console.log(token);
-    return http.delete(`/vi-tri/${id}`, {
-      headers: {
-        token: token,
-      },
-    });
-  },
-  capNhatviTri: (id, values) => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    const token = userInfo?.token;
-    console.log(token);
-    return http.put(`/vi-tri/${id}`, values, {
-      headers: {
-        token: token,
-      },
-    });
+  getViTriById: (id) => {
+    return http.get(`/vi-tri/${id}`);
   },
 };
