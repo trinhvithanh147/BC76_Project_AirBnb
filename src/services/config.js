a60e706d60aa6bd39953ef07a8a0e3012407b760
double2T <trinhvidanhthanh@gmail.com>
2026-06-16 19:09:55 +0700

    cap nhat lai token

--- src/services/config.js
+++ src/services/config.js
@@ -5,6 +5,6 @@ export const http = axios.create({
   timeout: 30000,
   headers: {
     tokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3NiIsIkhldEhhblN0cmluZyI6IjI1LzA0LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0NTUzOTIwMDAwMCIsIm5iZiI6MTcxNzA4ODQwMCwiZXhwIjoxNzQ1Njg2ODAwfQ.bh24gBD3xXGDrdB5FbpBMKC6PooFLXo00Pxus3IZcQQ",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3NiIsIkhldEhhblN0cmluZyI6IjI1LzA0LzIwMzAiLCJIZXRIYW5UaW1lIjoiMTc0NTUzOTIwMDAwMCIsIm5iZiI6MTcxNzA4ODQwMCwiZXhwIjoxNzQ1Njg2ODAwfQ.yy8aHWbOtw4ilJ0qdIgqQVut2TSF2s9Iuu0kgqbcYlg",
   },
 });
